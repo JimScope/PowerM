46a216b1902eefa3d2855e414805c95f1ea4ab2d
Jimmy Angel Pérez Díaz <mratom2307@gmail.com>
2019-05-23 11:22:14 -0400

    Update PowerM.py

--- PowerM.py
+++ PowerM.py
@@ -3,25 +3,23 @@
 from time import sleep
 import traceback
 
-
+# Local Modules
 import config
 import emailer
 import utilities
-# Importing Modules
+# Importing PowerM Modules
 from modules.test import multiply
 from modules.test import slow
+from modules.admin import logs
 
 
 def on_startup():
-    print("PowerMail")
     utilities.log("Starting up.")
     # Ensure the user has setup the script
     if config.email_user_name == "" or config.white_list == [] or config.email_password == "":
         utilities.log("Email variables are not setup. Exiting.")
         exit(1)
-
     utilities.log("Waiting for startup delay.")
-
 
 
 def read_commands():
@@ -29,15 +27,20 @@ def read_commands():
 
     #|
     if messages is None:
-        print("Empty")
+        # print("Empty")
+        pass
     else:
         for x in messages:
-            if x[1].startswith("$multiply "):
+            if x[1].startswith("$logs"):
+                thread_logs = logs.MyThread(x[0])
+                thread_logs.start()
+
+            if x[1].startswith("$multiply"):
                 keywords = x[1].lstrip("$multiply ")
                 thread_multiply = multiply.MyThread(x[0],keywords)
                 thread_multiply.start()
 
-            elif x[1].startswith("$slow "):
+            elif x[1].startswith("$slow"):
                 keywords = x[1].lstrip("$slow ")
                 thread_slow = slow.MyThread(x[0],keywords)
                 thread_slow.start()
@@ -45,20 +48,14 @@ def read_commands():
 
 def main():
     on_startup()
-
-    # Delays measured in seconds.
-    loop_delay = 15
-    iteration = 0
-
-    # Continuously monitor email for new commands, pausing every 42 seconds
     try:
+        print("Welcome to PowerM\nCtrl-C to exit")
+        print("Waiting for startup delay.")
         while True:
-            # Check for commands in emails every loop
-            print("Working...")
+            sleep(15)
             read_commands()
-            sleep(loop_delay)
-            iteration += 1
-    except:
+
+    except Exception as e:
         # In case of an uncaught exception, get stacktrace for diag and exit.
         trace_string = traceback.format_exc()
 
@@ -68,7 +65,7 @@ def main():
         # Build and send an email
         sub = "PowerMail crashed"
         msg = "Something went wrong with PowerMail, here is the stack trace:\n\n" + trace_string
-        emailer.send(sub, msg)
+        emailer.send(config.support, sub, msg)
 
         # Exit the program with error code 1
         exit(1)
